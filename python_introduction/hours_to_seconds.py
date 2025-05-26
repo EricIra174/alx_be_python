8f592d388a6865d1d945e4bafb11d4d536e2863b
EricIra174 <181249888+EricIra174@users.noreply.github.com>
2025-05-26 12:26:49 +0200

    Update hours_to_seconds.py

--- python_introduction/hours_to_seconds.py
+++ python_introduction/hours_to_seconds.py
@@ -1,12 +1,5 @@
 hours = 2
 seconds = hours * 3600
-seconds=hours*3600
-seconds = 3600 * hours
-seconds=3600*hours
-
-hours = int(input("Enter number of hours: "))
-seconds = hours * 3600
-print(f"2 hour(s) is {seconds} seconds.")
-
-# Output:
-# 2 hour(s) is 7200 seconds.
+print(f"{hours} hour(s) is {seconds} seconds.")
+#Output
+#2 hour(s) is 7200 seconds.
