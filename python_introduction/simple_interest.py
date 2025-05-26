ff5c44a1c3bd14bb6adcf1f7a9ef918197a226ed
EricIra174 <181249888+EricIra174@users.noreply.github.com>
2025-05-26 12:19:29 +0200

    Update simple_interest.py

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -1,12 +1,8 @@
-principle = 1000  # Principal amount
-rate = 5 # Rate of interest in percentage
-time = 3  # Time in years
+# This script calculates simple interest using the formula:
+principal = 1000
+rate = 0.05
+time = 5
 
-# Calculate simple interest
-simple_intrest = (principle * rate * time) / 100
+interest = principal * rate * time
+print(f"The simple interest is {interest}")
 
-# Print the result
-print(f"the simple interest is {simple_intrest}")
-
-# Output:
-# the simple interest is 150.0
